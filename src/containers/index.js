94702017de8aabf2d5a50a41f6a4f122d3979ba0
chendongpu <2323178881@qq.com>
2021-07-13 11:27:10 +0800

    添加点击返回按钮退出应用功能

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,7 +1,35 @@
 import React, { Component } from "react";
-import {Text, View} from 'react-native';
+import { connect } from "react-redux";
+import {Text,BackHandler, View, Alert} from 'react-native';
+import { NavigationActions } from 'react-navigation';
 
+@connect(({ test }) => ({
+    words: test.words
+}))
 export default class App extends Component {
+
+    componentDidMount() {
+        BackHandler.addEventListener("hardwareBackPress", this.onBackPress);
+    }
+
+    componentWillUnmount() {
+        BackHandler.removeEventListener("hardwareBackPress", this.onBackPress);
+    }
+
+    onBackPress = () => {
+        const { dispatch, navigation } = this.props;
+        Alert.alert(
+            '退出应用',
+            '确认退出应用吗?',
+            [
+                { text: '取消', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
+                { text: '确认', onPress: () => BackHandler.exitApp() },
+            ],
+            { cancelable: false }
+        );
+        return true;
+    };
+
     render() {
         return (
             <View>
